0dafa045f0b90709a51be0f17ba01e6c88fc6313
maher <gomemahero.com>
2022-06-03 14:54:26 +0300

    csrf protection start work endpoint

--- context/index.js
+++ context/index.js
@@ -1,5 +1,6 @@
 import { useReducer, createContext,useEffect } from "react";
-
+import axios from 'axios'
+import { useRouter, userRouter } from "next/router";
 // initial state
 const intialState = {
   user: null,
@@ -36,6 +37,50 @@ const Provider = ({ children }) => {
       payload: JSON.parse(window.localStorage.getItem("user")),
     });
   }, []);
+
+
+  axios.interceptors.response.use(
+    function (response) {
+      // any status code that lie within the range of 2XX cause this function
+      // to trigger
+      return response;
+    },
+    function (error) {
+      // any status codes that falls outside the range of 2xx cause this function
+      // to trigger
+      let res = error.response;
+      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+        return new Promise((resolve, reject) => {
+          axios
+            .get("/api/logout")
+            .then((data) => {
+              console.log("/401 error > logout");
+              dispatch({ type: "LOGOUT" });
+              window.localStorage.removeItem("user");
+              router.push("/login");
+            })
+            .catch((err) => {
+              console.log("AXIOS INTERCEPTORS ERR", err);
+              reject(error);
+            });
+        });
+      }
+      return Promise.reject(error);
+    }
+  );
+
+
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get("/api/csrf-token");
+      // console.log("CSRF", data);
+      axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
+    };
+    getCsrfToken();
+  }, []);
+
+
+
 
 
 
